aa299155614e4e9f37f1c9f08ffd710fdbf349f6
guenther.roll@gmail.com <guenther.roll@gmail.com@09724b84-3910-2db0-865c-5452d3ab954e>
2013-11-09 16:43:11 +0000

    Plugin import - not working yet

---
+++ trunk/LMS_Plugin/SBNetIO/SBNetIOSendMsg.pm
@@ -0,0 +1,336 @@
+#	SBNetIO
+#
+#	Author:	Günther Roll <guenther.roll(at)gmail(dot)com>
+#
+#	Copyright (c) 2013 Guenther Roll
+#	All rights reserved.
+#
+#	----------------------------------------------------------------------
+#	Function:	Send HTTP Commands to support SBNetIO plugin
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+#	02111-1307 USA
+#
+package Plugins::SBNetIO::SBNetIOSendMsg;
+
+use strict;
+use base qw(Slim::Networking::Async);
+
+use URI;
+use Slim::Utils::Log;
+use Slim::Utils::Misc;
+use Slim::Utils::Prefs;
+use Socket qw(:crlf);
+
+# ----------------------------------------------------------------------------
+my $log = Slim::Utils::Log->addLogCategory({
+	'category'     => 'plugin.sbnetio',
+	'defaultLevel' => 'ERROR',
+	'description'  => 'PLUGIN_SBNETIO_MODULE_NAME',
+});
+
+# ----------------------------------------------------------------------------
+# Global Variables
+# ----------------------------------------------------------------------------
+	my $prefs = preferences('plugin.sbnetio'); #name of preferences
+	my $self;
+	my $gGetPSModes=0;	# looping through the PS modes
+
+# ----------------------------------------------------------------------------
+# References to other classes
+# ----------------------------------------------------------------------------
+my $classPlugin		= undef;
+
+
+# ----------------------------------------------------------------------------
+sub new {
+	my $ref = shift;
+	$classPlugin = shift;
+
+	$log->debug( "*** SBNetIO::SBNetIOSendMsg::new() " . $classPlugin . "\n");
+	$self = $ref->SUPER::new;
+}
+
+
+# ----------------------------------------------------------------------------
+sub SendNetVolume{
+	my $client = shift;
+	my $url = shift;
+	my $vol = shift;
+	my $request = "Volume " .  $vol . $CR ;
+
+	$log->debug("Calling writemsg for volume command: $request");	
+	writemsg($request, $client, $url);
+}
+
+
+# ----------------------------------------------------------------------------
+sub SendNetPowerOn{
+	my $client = shift;
+	my $url = shift;
+	my $request = "PWON" . $CR;
+	my $timeout = 1;
+
+	$log->debug("Calling writemsg for Power On command");	
+	writemsg($request, $client, $url, $timeout);
+}
+
+
+# ----------------------------------------------------------------------------
+sub SendNetPowerOff{
+	my $client = shift;
+	my $url = shift;
+	my $request = "PWOFF" . $CR;
+	
+	$log->debug("Calling writemsg for Power Off command");
+	writemsg($request, $client, $url);
+}
+
+
+# ----------------------------------------------------------------------------
+sub writemsg {
+	my $request = shift;
+	my $client = shift;
+	my $url = shift;
+	my $timeout = shift;	
+
+#	$log->debug("Command url: " . $url);
+
+	my $u = URI->new($url);
+	my @pass = [ $request, $client ];
+
+	if (!$timeout) {
+		$timeout = .125;
+	}
+
+	$self->write_async( {
+		host        => $u->host,
+		port        => $u->port,
+		content_ref => \$request,
+		Timeout     => $timeout,
+		skipDNS     => 1,
+		onError     => \&_error,
+		onRead      => \&_read,
+		passthrough => [ $url, @pass ],
+		} );
+	$log->debug("Sent command request: " . $request);
+}
+
+
+# ----------------------------------------------------------------------------
+sub _error {
+	my $self  = shift;
+	my $errormsg = shift;
+	my $url   = shift;
+	my $track = shift;
+	my $args  = shift;
+	$log->debug("error routine called");
+
+	$self->disconnect;
+
+	my $error = "error connecting to url: error=$errormsg url=$url";
+	$log->warn($error);
+}
+
+
+# ----------------------------------------------------------------------------
+sub getCRLine($$) {
+	my $socket = shift;
+	my $maxWait = shift;
+	my $buffer = '';
+	my $start = Time::HiRes::time();
+	my $c;
+	my $r;
+	B: while ( (Time::HiRes::time() - $start) < $maxWait ) {
+		$r = $socket->read($c,1);
+		if ( $r < 1 ) { next B; }
+		$buffer .= $c;
+		if ( $c eq "\r" ) { return $buffer; }
+	}
+	return $buffer;
+}
+
+
+# ----------------------------------------------------------------------------
+sub _read {
+	my $self  = shift;
+	my $url   = shift;
+	my $track = shift;
+	my $args  = shift;
+
+	my $buf;
+	my @track = @$track;
+	my $i;
+	my $sSM;
+	my $request = @track[0];
+	my $client = @track[1];
+	my $len;
+	my $subEvent;
+	my $event;
+	my $callbackOK; 	# the returned message when the command was successful
+	my $callbackError; 	# the returned message when the command was not successful
+
+	$log->debug("read routine called");
+    $buf = &getCRLine($self->socket,.125);
+    my $read = length($buf);
+#	my $read = sysread($self->socket, $buf, 1024); # do our own sysread as $self->socket->sysread assumes http
+#	my $read = sysread($self->socket, $buf, 135); # do our own sysread as $self->socket->sysread assumes http
+
+	if ($read == 0) {
+		$callbackOK = "";
+		$self->_error("End of file", $url, $track, $args);
+		return;
+	} else {
+		$callbackOK = $buf;
+		$log->debug("Read ".$read."\n");
+	}
+
+	$log->debug("Buffer read ".$buf."\n");
+	$log->debug("Client name: " . $client->name . "\n");	
+	
+	$self->disconnect;
+	
+
+# #	if ($gGetPSModes == -1 || $gGetPSModes == 5) {
+# #		$log->debug("Disconnecting Comms Session. gGetPSModes:" . $gGetPSModes . "\n");		
+# #		Slim::Utils::Timers::killTimers( $client, \&SendTimerLoopRequest);
+# #		$self->disconnect;
+# #		$gGetPSModes =0;
+# #	}
+
+	# # see what is coming back from the AVP
+	# my $command = substr($request,0,3);
+	# $log->debug("Command is:" .$request);
+
+	# $log->debug("Subcommand is:" .$command. "\n");
+	# if ($request =~ m/PWON\r/ || $request =~ m/PW\?\r/) {	# power on or status
+		# if ($buf eq 'PWON'. $CR) {
+			# $self->disconnect;
+			# $log->debug("Calling HandlePowerOn\n");	
+			# $classPlugin->handlePowerOn($client);
+		# } elsif ($buf eq 'PWOFF'. $CR) {
+			# $log->debug("Calling HandlePowerOn\n");	
+			# $classPlugin->handlePowerOn($client);
+		# }
+	# }
+	# } elsif ($request =~ m/Z\d\?\r/) {	# zone power on
+		# $log->debug("Calling HandlePowerOn for Zone\n");	
+		# $classPlugin->handlePowerOn($client);
+	# } elsif (substr($request,0,7) eq 'MSQUICK') { # quick setting
+		# if ($buf eq 'PWON'. $CR) {
+			# $self->disconnect;
+			# SendNetAvpVolSetting($client, $url);
+		# }
+	# } elsif ($request =~ m/PWSTANDBY\r/) { #standby
+		# $log->debug("Disconnect socket after Standby"."\n");
+		# if ($buf eq 'PWSTANDBY'. $CR) {
+			# $self->disconnect;
+		# }
+	# } elsif ($request =~ m/MV\?/) {
+			# $log->debug("Volume setting inquiry"."\n");
+			# $event = substr($buf,0,2);
+			# if ($event eq 'MV') { #check to see if the element is a volume
+				# $subEvent = substr($buf,2,3);
+			  # if ($subEvent eq 'MAX') { # its not the one that tells us the volume change
+					# $self->disconnect;		
+				# } else {
+					# # call the plugin routine to deal with the volume
+					# $classPlugin->updateSqueezeVol($client, $subEvent);
+				# }
+			# }
+	# } elsif ($request =~ m/MV/) {
+			# $log->debug("Process Volume Setting"."\n");
+			# $self->disconnect;
+	# } elsif ($request =~ m/MS\?/ || $request =~ m/PS\?/ || $request =~ m/PSREFLEV\s\?/) {
+		# my @events = split(/\r/,$buf); #break string into array
+		# foreach $event (@events) { # loop through the event array parts
+			# $log->debug("The value of the array element is: " . $event . "\n");			
+			# $command = substr($event,0,2);
+			# if ($command eq 'MS') { #check to see if the element is a surround mode
+				# $i=0;
+				# $subEvent = substr($events[0],0,5);
+				# foreach (@surroundModes) {
+					# $sSM = substr($surroundModes[$i],0,5);
+					# if ($subEvent eq $sSM || ((substr($events[0],3,2) eq "CH") && ($sSM eq "MS7CH"))) {
+						# # call the surround mode plugin routine to set the value
+						# $log->debug("Surround Mode is: " . $surroundModes[$i] . "\n");
+						# $classPlugin->updateSurroundMode($client, $i);
+					# }
+					# $i++;
+				# } # foreach (@surroundModes)
+			# } elsif ($command eq 'PS') { #check to see if the element is a PS mode
+				# $subEvent = substr($events[0],0,6);
+				# if ( $subEvent eq 'PSROOM') { #room modes
+					# $i=0;
+					# foreach (@roomModes) {
+						# if ($roomModes[$i] eq $events[0]) {
+							# # call the room mode plugin routine to set the value
+							# $log->debug("Room Mode is: " . $roomModes[$i] . "\n");
+							# $classPlugin->updateRoomEq($client, $i);
+						# } # if
+						# $i++;
+					# } # foreach roomModes
+				# } elsif ($subEvent eq 'PSDYNS') { # night mode
+					# $i=0;
+					# foreach (@nightModes) {
+						# if ($nightModes[$i] eq $events[0]) {
+							# # call the night mode plugin routine to set the value
+							# $log->debug("Night Mode is: " . $nightModes[$i] . "\n");
+							# $classPlugin->updateNM($client, $i);
+						# } # if
+						# $i++;
+					# } # foreach nightModes
+				# } elsif ($subEvent eq 'PSDYN ') { # dynamic volume
+					# $i=0;
+					# foreach (@dynamicVolModes) {
+						# if ($dynamicVolModes[$i] eq $events[0]) {
+							# # call the dynamic vol mode plugin routine to set the value
+							# $log->debug("Dynamic Volume Mode is: " . $dynamicVolModes[$i] . "\n");
+							# $classPlugin->updateDynEq($client, $i);
+						# } # if
+						# $i++;
+					# } # foreach dynamicVolModes
+				# } elsif ($subEvent eq 'PSRSTR') { # restorer
+					# $i=0;
+					# foreach (@restorerModes) {
+						# if ($restorerModes[$i] eq $events[0])  {
+							# # call the restorer mode plugin routine to set the value
+							# $log->debug("Restorer Mode is: " . $restorerModes[$i] . "\n");
+							# $classPlugin->updateRestorer($client, $i);
+						# } # if
+						# $i++;
+					# } # foreach restorerModes
+				# } elsif ($subEvent eq 'PSREFL') { # reference level
+					# $i=0;
+					# foreach (@refLevelModes) {
+						# if ($refLevelModes[$i] eq $events[0])  {
+							# # call the refence level plugin routine to set the value
+							# $log->debug("Reference level is: " . $refLevelModes[$i] . "\n");
+							# $classPlugin->updateRefLevel($client, $i);
+						# } # if
+						# $i++;
+					# } # foreach refLevelModes
+				# }
+			# }
+		# } # foreach (@events)
+		# # now see if we should loop the AVP settings
+# #		if ($gGetPSModes !=0) {
+# #			$gGetPSModes++;
+# #			LoopGetAvpSettings($client, $url);
+# #		}
+	# } # if ($request =~ /PWON\r/) {	# power on ...
+
+} # _read
+
+1;
---
+++ trunk/LMS_Plugin/SBNetIO/Settings.pm
@@ -0,0 +1,173 @@
+package Plugins::SBNetIO::Settings;
+
+# SqueezeCenter Copyright (c) 2001-2009 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License, 
+# version 2.
+
+use strict;
+use base qw(Slim::Web::Settings); #driven by the web UI
+
+use Slim::Utils::Strings qw(string); #we want to use text from the strings file
+use Slim::Utils::Log; #we want to use the log methods
+use Slim::Utils::Prefs; #we want access to the preferences methods
+
+# ----------------------------------------------------------------------------
+# Global variables
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# References to other classes
+# ----------------------------------------------------------------------------
+my $classPlugin		= undef;
+
+# ----------------------------------------------------------------------------
+my $log = Slim::Utils::Log->addLogCategory({
+	'category'     => 'plugin.sbnetio',
+	'defaultLevel' => 'ERROR',
+	'description'  => 'PLUGIN_SBNETIO_MODULE_NAME',
+});
+
+# ----------------------------------------------------------------------------
+my $prefs = preferences('plugin.sbnetio'); #name of preferences
+
+# ----------------------------------------------------------------------------
+# Define own constructor
+# - to save references to Plugin.pm
+# ----------------------------------------------------------------------------
+sub new {
+	my $class = shift;
+
+	$classPlugin = shift;
+
+	$log->debug( "*** SBNetIO::Settings::new() " . $classPlugin . "\n");
+
+	$class->SUPER::new();	
+
+	return $class;
+}
+
+# ----------------------------------------------------------------------------
+# Name in the settings dropdown
+# ----------------------------------------------------------------------------
+sub name { #this is what is shown in the players menu on the web gui
+	return 'PLUGIN_SBNETIO_MODULE_NAME';
+}
+
+# ----------------------------------------------------------------------------
+# Webpage served for settings
+# ----------------------------------------------------------------------------
+sub page { #tells which file to use as the web page
+	return 'plugins/SBNetIO/settings/basic.html';
+}
+
+# ----------------------------------------------------------------------------
+# Settings are per player
+# ----------------------------------------------------------------------------
+sub needsClient {
+	return 1; #this means this is for a particular squeezebox, not the system
+}
+
+# ----------------------------------------------------------------------------
+# Only show plugin for Squeezebox 3 or Receiver players
+# ----------------------------------------------------------------------------
+sub validFor {
+	my $class = shift;
+	my $client = shift;
+	# Receiver and Squeezebox2 also means SB3
+	return $client->isPlayer && ($client->isa('Slim::Player::Receiver') || 
+		                         $client->isa('Slim::Player::Squeezebox2') ||
+		                         $client->isa('Slim::Player::SqueezeSlave'));
+}
+
+# ----------------------------------------------------------------------------
+# Handler for settings page
+# ----------------------------------------------------------------------------
+sub handler {
+	my ($class, $client, $params) = @_; 
+	#passes the class and client objects along with the parameters
+
+	# $client is the client that is selected on the right side of the web interface!!!
+	# We need the client identified by 'playerid'
+
+	# Find player that fits the mac address supplied in $params->{'playerid'}
+	my @playerItems = Slim::Player::Client::clients();
+	foreach my $play (@playerItems) {
+		if( $params->{'playerid'} eq $play->macaddress()) {
+			$client = $play; #this particular player
+			last;
+		}
+	}
+	if( !defined( $client)) {
+		#set the class object with the particular player
+		return $class->SUPER::handler($client, $params); 
+		$log->debug( "*** SBNetIO: found player: " . $client . "\n");
+	}
+
+	
+	# Fill in name of player
+	if( !$params->{'playername'}) {
+		#get the player name but I don't use it
+		$params->{'playername'} = $client->name(); 
+		$log->debug( "*** SBNetIO: player name: " . $params->{'playername'} . "\n");
+	}
+	
+	# set a few defaults for the first time
+	if ($prefs->client($client)->get('delayOn') == '') {
+		$prefs->client($client)->set('delayOn', '0');
+	} 
+	if ($prefs->client($client)->get('delayOff') == '') {
+		$prefs->client($client)->set('delayOff', '0');
+	}
+
+	# When "Save" is pressed on the settings page, this function gets called.
+	if ($params->{'saveSettings'}) {
+		#store the enabled value in the client prefs
+		if ($params->{'pref_Enabled'}){ #save the enabled state
+			$prefs->client($client)->set('pref_Enabled', 1); 
+		} else {
+			$prefs->client($client)->set('pref_Enabled', 0);
+		}
+		if ($params->{'srvAddress'}) { #save the Server IP Address
+			my $srvAddress = $params->{'srvAddress'};
+			# get rid of leading spaces if any since one is always added.
+			$srvAddress =~ s/^\s+(.*)\s+/\1/;
+			#save the AVP address in the client prefs
+			$prefs->client($client)->set('srvAddress', "$srvAddress"); 
+		}
+		if ($params->{'delayOn'} =~ /^-?\d/) { #save the delay on time
+			my $delayOn = $params->{'delayOn'};
+			# get rid of leading spaces if any since one is always added.
+			$delayOn =~ s/^\s+(.*)\s+/\1/;
+			#save the delay on time in the client prefs
+			$prefs->client($client)->set('delayOn', "$delayOn"); 
+		}
+		if ($params->{'delayOff'} =~ /^-?\d/) { #save the delay off time
+			my $delayOff = $params->{'delayOff'};
+			# get rid of leading spaces if any since one is always added.
+			$delayOff =~ s/^\s+(.*)\s+/\1/;
+			#save the delay off time in the client prefs
+			$prefs->client($client)->set('delayOff', "$delayOff"); 
+		}
+	}
+
+	# Puts the values on the webpage. 
+	#next line takes the stored plugin pref value and puts it on the web page
+	#set the enabled checkbox on the web page
+	if ($prefs->client($client)->get('pref_Enabled') == '1') {
+		$params->{'prefs'}->{'pref_Enabled'} = 1; 
+	}
+
+	# this puts the text fields in the web page
+	$params->{'prefs'}->{'srvAddress'} = $prefs->client($client)->get('srvAddress'); 
+	$params->{'prefs'}->{'delayOn'} = $prefs->client($client)->get('delayOn'); 
+	$params->{'prefs'}->{'delayOff'} = $prefs->client($client)->get('delayOff'); 
+	
+	return $class->SUPER::handler($client, $params);
+}
+
+1;
+
+__END__
+
+pref_Enabled
